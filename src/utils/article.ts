be9970fd33392f0468086141b978751f220b108c
Ahn Heejong <heejongahn@gmail.com>
2024-11-17 15:53:33 +0900

    Declare Article type

--- src/utils/article.ts
+++ src/utils/article.ts
@@ -1,7 +1,7 @@
 import type { CollectionEntry } from "astro:content";
 import { isBefore } from "date-fns";
 
-export const compareArticle = (
-  a: CollectionEntry<"article">,
-  b: CollectionEntry<"article">
-): number => (isBefore(a.data.date, b.data.date) ? 1 : -1);
+export type Article = CollectionEntry<"article">;
+
+export const compareArticle = (a: Article, b: Article): number =>
+  isBefore(a.data.date, b.data.date) ? 1 : -1;
