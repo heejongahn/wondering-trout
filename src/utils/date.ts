8f4323466f9bd5c02c48882f61054c5909f62a42
Ahn Heejong <heejongahn@gmail.com>
2026-07-11 15:26:02 +0900

    style fixes

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,11 +1,12 @@
 import { parseISO, format } from "date-fns";
+import { ko } from "date-fns/locale";
 
 export function parseDate(dateString: string) {
   return parseISO(dateString);
 }
 
 export function formatDate(date: Date, dateFormat?: string) {
-  return format(date, dateFormat ?? `yyyy. M. d`);
+  return format(date, dateFormat ?? `PPP`, { locale: ko });
 }
 
 export function prettifyDate(date: Date, dateFormat?: string) {
